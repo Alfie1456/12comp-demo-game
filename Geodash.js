713842c02506e450384c7a0d410d5742b7c76607
Alfie1456 <161382522+Alfie1456@users.noreply.github.com>
2024-02-27 10:52:38 +1300

    Update Geodash.js
    Jumping is working

--- Geodash.js
+++ Geodash.js
@@ -16,8 +16,20 @@ yellowBox = new Sprite(100, 100, 30, 30, 'd');
   yellowBox.color = 'yellow';
   wall = new Sprite(0, height, windowWidth*2, 15, 'k');
   wall.color = 'lightGreen';
+
   
+  document.addEventListener("keydown",
+  function(event) {
+    console.log ("key pressed" +yellowBox.y);
+    if(yellowBox.y > 184){
+        console.log = ("key pressed!");
+    yellowBox.vel.y = -5;
+  }
   
+});
+
+
+
 }
 /*******************************************************/
 // draw()
